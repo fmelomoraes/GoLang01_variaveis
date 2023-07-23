ff480ca876f79b81999b0532608e557edb36d60d
Fabiano Moraes <90139746+fmelomoraes@users.noreply.github.com>
2023-07-23 15:56:54 -0300

    GoLang01
    Iniciando a Linguagem GO

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Fabiano"
+	var idade int = 39
+	var versao float32 = 1.1
+	fmt.Println("Olá, me chamo", nome, "e tenho", idade, "anos")
+	fmt.Println("Este programa está na versão:", versao)
+}
